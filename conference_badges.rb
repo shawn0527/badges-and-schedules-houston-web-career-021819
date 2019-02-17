d904c620b23af5d6a2616e1857338a876a5cd07e
Shawn Ren <doggy-ren@hotmail.com>
2019-02-17 23:53:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,4 @@
+# Write your code here.
 def badge_maker(name)
   "Hello, my name is #{name}."
-end
-# Write your code here.
-def batch_badge_creator(attendees)
-  index = 0
-  while index < attendeess.length
-  name = attendeess[index]
-  badge_maker(name)
-  index += 1
-end
 end
