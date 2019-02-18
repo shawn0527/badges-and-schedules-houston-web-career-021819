28c2ef6ef9b46e81fdd11542b36f5e26bd97d290
Shawn Ren <doggy-ren@hotmail.com>
2019-02-18 00:03:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(attendees)
   index = 0
   while index < attendees.length
-  return "Hello, my name is #{attendees[index]}"
+  puts "Hello, my name is #{attendees[index]}"
     index += 1
   end
 end
