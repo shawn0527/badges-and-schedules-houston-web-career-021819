126baf78e066c86a28aae1a109dcf020894728cb
Shawn Ren <doggy-ren@hotmail.com>
2019-02-18 00:55:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
 end
