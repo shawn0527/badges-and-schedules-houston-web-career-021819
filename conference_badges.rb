78ceeac2790fe372d01590a8e149684df14508a3
Shawn Ren <doggy-ren@hotmail.com>
2019-02-18 00:37:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(attendees)
   newarray=[]
   index = 0
   while index < attendees.length
-    newarray.push(badge_maker(attnedees[index])
+    newarray.push(badge_maker(attnedees[index]))
     index+=1
   end
   return newarray
